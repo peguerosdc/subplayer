9429ed359605f2628451dea0d2a02e8d3aea6330
peguerosdc <peguerosdc@pdc.local>
2019-07-20 18:50:03 -0500

    Adding logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,18 @@ import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 
+/**
+ * Logs all actions and states after they are dispatched.
+ */
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
+
 export default function configureStore(initialState) {
     const composeEnhancers =
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
@@ -10,6 +22,6 @@ export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+        composeEnhancers(applyMiddleware(thunk, logger, reduxImmutableStateInvariant()))
     );
 }
