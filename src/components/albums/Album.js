4c6634711c131899651725368a0e79725378d8b1
peguerosdc <peguerosdc@pdc.local>
2019-07-26 14:20:59 -0500

    Optimizing grid in Album's mobile view

--- src/components/albums/Album.js
+++ src/components/albums/Album.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import subsonic from "../../api/subsonicApi";
 import { addSongsToPlaylist } from "../../redux/actions/playlistsActions";
 // UI
-import { FlexboxGrid, Panel } from 'rsuite';
+import { Grid, Row, Col, Panel } from 'rsuite';
 import SongsTable from '../songs/SongsTable'
 import PlaylistSelectorDropdown from '../common/PlaylistSelectorDropdown.js'
 
@@ -39,16 +39,21 @@ class Album extends React.Component {
         return (
             album.name ? (
                 <Panel bordered style={{backgroundColor:"white", ...this.props.style}}>
-                    <FlexboxGrid>
-                        <FlexboxGrid.Item colspan={6}>
-                            <img src={album.coverArt ? subsonic.getCoverArtUrl(album.coverArt) : null} alt="Album Cover" width="100%" />
-                        </FlexboxGrid.Item>
-                        <FlexboxGrid.Item colspan={18} style={{paddingLeft:"10px"}}>
-                            <h2>{album ? album.name : "..."}</h2>
-                            <PlaylistSelectorDropdown onPlaylistSelected={this.onPlaylistSelected} disabled={disableDropdown} />
-                            <SongsTable songs={songs} columns={[SongsTable.columns.title, SongsTable.columns.duration, SongsTable.columns.bitRate, SongsTable.columns.selectable]} onSongsSelected={this.onSongsSelected} />
-                        </FlexboxGrid.Item>
-                    </FlexboxGrid>
+                    <Grid fluid>
+                        <Row>
+                            <Col smHidden xsHidden md={6} lg={6}>
+                                <img src={album.coverArt ? subsonic.getCoverArtUrl(album.coverArt) : null} alt="Album Cover" width="100%" />
+                            </Col>
+                            <Col sm={24} md={18} style={{paddingLeft:"10px"}}>
+                                <h2>
+                                    <img className="rs-hidden-md rs-hidden-lg" src={album.coverArt ? subsonic.getCoverArtUrl(album.coverArt) : null} alt="Album Cover" width="45" height="45" />
+                                    {album ? album.name : "..."}
+                                </h2>
+                                <PlaylistSelectorDropdown onPlaylistSelected={this.onPlaylistSelected} disabled={disableDropdown} />
+                                <SongsTable songs={songs} columns={[SongsTable.columns.title, SongsTable.columns.duration, SongsTable.columns.bitRate, SongsTable.columns.selectable]} onSongsSelected={this.onSongsSelected} />
+                            </Col>
+                        </Row>
+                    </Grid>
                 </Panel>
                 ) : null
         )
