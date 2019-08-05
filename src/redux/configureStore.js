1cd664235988517e30432488b9aadee9fba0d98a
peguerosdc <peguerosdc@pdc.local>
2019-08-05 17:23:05 -0500

    Removing logs from PROD

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,26 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux"
-import rootReducer from "./reducers"
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant"
-import thunk from "redux-thunk"
-
-/**
- * Logs all actions and states after they are dispatched.
- */
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd()
-  return result
+// Use CommonJS require below so we can dynamically import during build-time.
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./configureStore.prod");
 }
-
-export default function configureStore(initialState) {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // add support for Redux dev tools
-
-    return createStore(
-        rootReducer,
-        initialState,
-        composeEnhancers(applyMiddleware(thunk, logger, reduxImmutableStateInvariant()))
-    )
+else {
+    module.exports = require("./configureStore.dev");
 }
