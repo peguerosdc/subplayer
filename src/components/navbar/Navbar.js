b31df8495b23a79c5e29df73a361a7439224c41b
peguerosdc <peguerosdc@gmail.com>
2020-06-28 20:54:13 -0500

    Using Location for Navbar's activeKey

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,16 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 // Redux
 import { connect } from "react-redux"
-import { navigate } from "@reach/router"
+import { navigate, Location } from "@reach/router"
 // UI
 import {Navbar, Icon, Nav, Dropdown } from 'rsuite'
 
 export class MyNavbar extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = { path : "/latest/" }
-    }
 
     onNavSelected = (key) => {
         switch(key) {
@@ -23,36 +18,39 @@ export class MyNavbar extends React.Component {
                 break
             default:
                 navigate(key)
-                this.setState({path : key})
         }
-    }
-
-    isRouteActive = (route) => {
-        return this.state.path.startsWith(route)
     }
 
     render() {
         const playlists = this.props.playlists
         return (
-            <Navbar>
-                <Navbar.Body>
-                    <Nav onSelect={this.onNavSelected}>
-                        <Nav.Item id="search" eventKey="/search" icon={<Icon icon="search" />} active={this.isRouteActive("/search")} />
-                        <Nav.Item id="latest" eventKey="/latest/" icon={<Icon icon="clock-o" />} active={this.isRouteActive("/latest")} />
-                        <Nav.Item id="artists" eventKey="/artists/" icon={<Icon icon="group" />} active={this.isRouteActive("/artists")} />
-                        <Nav.Item id="favourites" eventKey="/favourites/" icon={<Icon icon="star" />} active={this.isRouteActive("/favourites")} />
-                        <Dropdown id="playlists" title="Playlists">
-                            <Dropdown.Item id="createPlaylist" eventKey="newPlaylist" icon={<Icon icon="plus" />} >New playlist</Dropdown.Item>
-                            {Object.keys(playlists).map( id =>
-                                <Dropdown.Item active={this.isRouteActive(`/playlist/${id}`)} key={id} eventKey={`/playlist/${id}`}>{playlists[id].name} ({playlists[id].songCount})</Dropdown.Item>
-                            )}
-                        </Dropdown>
-                    </Nav>
-                    <Nav onSelect={this.onNavSelected} pullRight>
-                        <Nav.Item id="logout" eventKey="logout" icon={<Icon icon="sign-out" />} />
-                    </Nav>
-                </Navbar.Body>
-            </Navbar>
+            <Location>
+                {props => {
+                    // Get the location from reach's <Location/> to highlight the active item
+                    const currentPath = props.location.pathname
+                    return (
+                        <Navbar>
+                            <Navbar.Body>
+                                <Nav activeKey={currentPath} onSelect={this.onNavSelected}>
+                                    <Nav.Item id="search" eventKey="/search" icon={<Icon icon="search" />} />
+                                    <Nav.Item id="latest" eventKey="/latest/" icon={<Icon icon="clock-o" />} />
+                                    <Nav.Item id="artists" eventKey="/artists/" icon={<Icon icon="group" />} />
+                                    <Nav.Item id="favourites" eventKey="/favourites/" icon={<Icon icon="star" />} />
+                                    <Dropdown id="playlists" title="Playlists">
+                                        <Dropdown.Item id="createPlaylist" eventKey="newPlaylist" icon={<Icon icon="plus" />} >New playlist</Dropdown.Item>
+                                        {Object.keys(playlists).map( id =>
+                                            <Dropdown.Item key={id} eventKey={`/playlist/${id}`}>{playlists[id].name} ({playlists[id].songCount})</Dropdown.Item>
+                                        )}
+                                    </Dropdown>
+                                </Nav>
+                                <Nav onSelect={this.onNavSelected} pullRight>
+                                    <Nav.Item id="logout" eventKey="logout" icon={<Icon icon="sign-out" />} />
+                                </Nav>
+                            </Navbar.Body>
+                        </Navbar>
+                    )}
+                }
+            </Location>
         )
     }
 }
