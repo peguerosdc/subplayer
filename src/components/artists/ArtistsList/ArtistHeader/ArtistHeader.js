897d2121d2fca6fe9252f49b3514660b17b5a3bd
peguerosdc <peguerosdc@gmail.com>
2020-06-28 23:10:00 -0500

    Theming artists list

--- src/components/artists/ArtistsList/ArtistHeader/ArtistHeader.js
+++ src/components/artists/ArtistsList/ArtistHeader/ArtistHeader.js
@@ -1,14 +1,13 @@
 import React from "react"
 import PropTypes from 'prop-types'
 // UI
-import "./ArtistHeader.less"
 import { Col } from 'rsuite'
 
 function ArtistHeader(props) {
     const { name } = props
     return (
     	<Col key={name} sm={24} xs={24} >
-	        <h2 id="title" className="link_to_artist_header">{name}</h2>
+	        <h4 id="title" className="artist-header">{name}</h4>
         </Col>
     )
 }
--- themes.content.js
+++ themes.content.js
@@ -9,4 +9,21 @@ module.exports = (themeName) => `
 	background-color: ${themeName === "dark" ? "@B700" : "#fff"};
 	border-top : ${themeName === "dark" ? "none" : "1px solid #f0f0f0"};
 }
+
+.artist-header {
+	color: ${themeName === "dark" ? "@base-color" : "inherit"};
+}
+
+.currently-playing {
+	color: @base-color;
+	font-weight: bold;
+}
+
+.link_to_artist:hover {
+    background-color: @nav-item-default-hover-bg;
+    cursor: pointer;
+    color: @base-color;
+    font-weight: bold;
+}
+
 `
