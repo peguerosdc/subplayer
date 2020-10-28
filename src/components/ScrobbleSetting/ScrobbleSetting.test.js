e2a72b7d98aa9771c3ed34d07080a5ddf40223ba
peguerosdc <peguerosdc@gmail.com>
2020-10-27 23:04:51 -0600

    Fixing ScrobbleSettings

--- src/components/ScrobbleSetting/ScrobbleSetting.test.js
+++ src/components/ScrobbleSetting/ScrobbleSetting.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import {ScrobbleSetting} from "./ScrobbleSetting"
+import ScrobbleSetting from "./ScrobbleSetting"
 
 describe("<ScrobbleSetting />", () => {
 
